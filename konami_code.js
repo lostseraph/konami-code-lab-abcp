b46f8ed5bc9108720bbd947afcec1cac5c218ce4
Roscoe Sabale <rsabale43@gmail.com>
2018-05-14 17:09:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
   var index = 0;
   document.body.addEventListener('keydown', function(e) {
-    const key = e.key;
+    const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
       if (index === code.length) {
